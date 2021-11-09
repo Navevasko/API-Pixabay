aaf6dfde081d473de269cf71bec855353202affa
Guilherme Navevaiko <guilherme_navevaiko@hotmail.com>
2021-11-08 22:23:55 -0300

    Somativa finalizada

--- js/script.js
+++ js/script.js
@@ -1,51 +1,72 @@
-"use strict"
-
-const imagens = [
-    "https://noticias.maisesports.com.br/wp-content/uploads/2021/10/LoL-Blitzcrank-Vitorioso-scaled.jpg",
-    "../galeria/img/Paisagem1.jpg",
-    "../galeria/img/Paisagem2.jpg",
-    "../galeria/img/Paisagem3.jpg",
-    "../galeria/img/Paisagem4.jpg",
-    "../galeria/img/Paisagem5.jpg",
-    "../galeria/img/Paisagem6.jpg",
-    "../galeria/img/Paisagem7.jpg",
-    "../galeria/img/Paisagem8.jpg"
-]
-
-const pesquisarImagens = async(evento) => {
-
-    if(evento.key === "Enter") {
-        const imagem = evento.target.value
-        const url = `https://pixabay.com/api/${imagem}`
-        const imagensResponse = await fetch(url)
-
-        if(imagensResponse.ok) {
-            const imagens = await imagensResponse.json()
-
-            carregarGaleria(imagens.hit)
-        }
-        else {
-            alert('Erro')
-        }
+"use strict";
+const limparElemento = elemento =>{
+    while(elemento.firstChild){
+        elemento.removeChild(elemento.lastChild);
     }
 }
 
-const filtrarId = (urlImagem) => {
-    const ultimaBarra = urlImagem.lastIndexOf("/") + 1
-    const ultimoPonto = urlImagem.lastIndexOf(".")
-    const url = urlImagem.substring(ultimaBarra, ultimoPonto)
-    return url
+const carregarStatus = (nImagens,pesquisa) =>{
+    const container = document.querySelector(".status");
+    const newStatus = document.createElement("p");
+    newStatus.classList = ".nFotos";
+    newStatus.innerHTML = `${nImagens.totalHits} fotos de ${pesquisa} encontradas`;
+    container.appendChild(newStatus);
 }
 
-const criarItem = (urlImagem) => {
-    const container = document.querySelector(".galeria-container")
-    const novaImagem = document.createElement("div")
-    novaImagem.innerHTML = `<img class="img-item" src="${urlImagem.webformatURL}" alt="">`
-    container.append(novaImagem)
+const pesquisarImagens = async(event) =>{
+
+    if(event.target.value == "") {
+        event.target.placeholder = "Digite algo"
+        limparElemento(document.querySelector("#container-galeria"));
+        limparElemento(document.querySelector(".status"));
+    }
+    else {
+        if(event.key == "Enter"){
+            const tipoImagem = document.querySelector("#categorias").value;
+            const imagem = event.target.value;
+            const url = `https://pixabay.com/api/?key=24046844-849812f8b5ea3813c855149ee&q=${imagem}&image_type=${tipoImagem}`;
+            const response = await fetch(url);
+
+            const imagens = await response.json();
+
+            limparElemento(document.querySelector("#container-galeria"));
+            limparElemento(document.querySelector(".status"));
+
+            carregarGaleria(imagens.hits);
+            carregarStatus(imagens,imagem);
+        }
+    }
+}
+const criarItem = item =>{
+
+    if(item.userImageURL == "") {
+        item.userImageURL = "img/user.png"
+    }
+
+    const container = document.querySelector("#container-galeria");
+    const card =document.createElement("div");
+    card.innerHTML = `
+                <a class="img-perfil" href="https://pixabay.com/users/${item.user}-${item.user_id}/">
+                    <img class="img-perfil" src="${item.userImageURL}">
+                </a>
+                <div class="options">
+                    <div class="info">${item.tags}</div>
+                    <div class="container-info">
+                        <div class="info"><img src="img/heart.png">${item.likes}</div>
+                        <div class="info"><img src="img/comment.png">${item.comments}</div>
+                        <div class="info"><img src="img/bookmark.png"></div>
+                    </div>
+                </div>
+                <a class="card-image"href="${item.pageURL}">
+                <img class="card-image" src="${item.webformatURL}">
+                </a>
+            
+            `;
+    container.appendChild(card);
 }
 
-const carregarGaleria = (imgs) => imgs.forEach(criarItem)
 
-document.querySelector('#pesquisar').addEventListener('keypress', pesquisarImagens)
+const carregarGaleria = imagens => imagens.forEach(criarItem);
 
-
+document.querySelector("#pesquisa").addEventListener("keypress", pesquisarImagens);
+document.querySelector("#categorias").addEventListener("slected", pesquisarImagens);
