5cfdbb6035fb410e778d023cc5b71df9bcb17854
GUILHERME NAVEVAIKO MOREIRA <21193389@CFP127ALUNO.LOCAL>
2021-11-09 15:55:06 -0300

    Pesquisa por vídeos concluída

--- js/script.js
+++ js/script.js
@@ -5,12 +5,31 @@ const limparElemento = elemento =>{
     }
 }
 
-const carregarStatus = (nImagens,pesquisa) =>{
+const carregarStatus = (nImagens,pesquisa, tipoItem) =>{
     const container = document.querySelector(".status");
     const newStatus = document.createElement("p");
     newStatus.classList = ".nFotos";
-    newStatus.innerHTML = `${nImagens.totalHits} fotos de ${pesquisa} encontradas`;
+    newStatus.innerHTML = `${nImagens.totalHits} ${tipoItem} de ${pesquisa} encontradas`;
     container.appendChild(newStatus);
+}
+
+const tipoPesquisa = () => {
+    const tipoPesquisa = document.querySelector('#categorias').value
+
+    if(tipoPesquisa == "all" || tipoPesquisa == "photo" || tipoPesquisa == "illustration" || tipoPesquisa == "vector") {
+        var url = `image_type=${tipoPesquisa}`
+        var video = ""
+        var type = "imagens"
+    }
+    else {
+        var url = `video_type=${tipoPesquisa}&per_page=9`
+        var video = "videos/"
+        var type = "videos"
+    }
+
+    const search  = [tipoPesquisa, url, video, type]
+
+    return search
 }
 
 const pesquisarImagens = async(event) =>{
@@ -22,9 +41,9 @@ const pesquisarImagens = async(event) =>{
     }
     else {
         if(event.key == "Enter"){
-            const tipoImagem = document.querySelector("#categorias").value;
+            const tipoImagem = tipoPesquisa()
             const imagem = event.target.value;
-            const url = `https://pixabay.com/api/?key=24046844-849812f8b5ea3813c855149ee&q=${imagem}&image_type=${tipoImagem}`;
+            const url = `https://pixabay.com/api/${tipoImagem[2]}?key=24046844-849812f8b5ea3813c855149ee&q=${imagem}&${tipoImagem[1]}`;
             const response = await fetch(url);
 
             const imagens = await response.json();
@@ -33,40 +52,58 @@ const pesquisarImagens = async(event) =>{
             limparElemento(document.querySelector(".status"));
 
             carregarGaleria(imagens.hits);
-            carregarStatus(imagens,imagem);
+            carregarStatus(imagens,imagem,tipoImagem[3]);
         }
     }
 }
-const criarItem = item =>{
+const criarImagem = imagem =>{
 
-    if(item.userImageURL == "") {
-        item.userImageURL = "img/user.png"
+    if(imagem.userImageURL == "") {
+        imagem.userImageURL = "img/user.png"
     }
 
+    const tipoPesquisa = document.querySelector('#categorias').value
     const container = document.querySelector("#container-galeria");
     const card =document.createElement("div");
+
+    if(tipoPesquisa == "videos") {
+        var tipoItem = `
+                <a class="card-image" href="${imagem.pageURL}">
+                    <video class="video" autoplay loop mute>
+                        <source class="video" src="${imagem.videos.small.url}">
+                    </video>
+                </a>
+            `
+        }
+        else {
+    
+            var tipoItem = `
+            <a class="card-image" href="${imagem.pageURL}">
+                <img class="card-image" src="${imagem.webformatURL}">
+            </a>
+        `
+        }
+
     card.innerHTML = `
-                <a class="img-perfil" href="https://pixabay.com/users/${item.user}-${item.user_id}/">
-                    <img class="img-perfil" src="${item.userImageURL}">
+                <a class="img-perfil" href="https://pixabay.com/users/${imagem.user}-${imagem.user_id}/">
+                    <img class="img-perfil" src="${imagem.userImageURL}">
                 </a>
                 <div class="options">
-                    <div class="info">${item.tags}</div>
+                    <div class="info">${imagem.tags}</div>
                     <div class="container-info">
-                        <div class="info"><img src="img/heart.png">${item.likes}</div>
-                        <div class="info"><img src="img/comment.png">${item.comments}</div>
+                        <div class="info"><img src="img/heart.png">${imagem.likes}</div>
+                        <div class="info"><img src="img/comment.png">${imagem.comments}</div>
                         <div class="info"><img src="img/bookmark.png"></div>
                     </div>
                 </div>
-                <a class="card-image"href="${item.pageURL}">
-                <img class="card-image" src="${item.webformatURL}">
-                </a>
+                ${tipoItem}
             
-            `;
+            `
     container.appendChild(card);
 }
 
 
-const carregarGaleria = imagens => imagens.forEach(criarItem);
+const carregarGaleria = imagens => imagens.forEach(criarImagem);
 
 document.querySelector("#pesquisa").addEventListener("keypress", pesquisarImagens);
 document.querySelector("#categorias").addEventListener("slected", pesquisarImagens);
